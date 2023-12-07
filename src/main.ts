6408553782d645f125f5ef69fa42bd1155c15e37
Gnlow <gnlow@protonmail.com>
2023-12-07 11:11:48 +0900

    use typescript

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import Blockly from "https://esm.sh/v135/blockly@10.2.2"
+
+Blockly.inject("blocklyDiv", {
+    toolbox: {
+        kind: "categoryToolbox",
+        contents: [
+            {
+                kind: "category",
+                name: "Core",
+                contents: [
+                    {
+                        kind: "block",
+                        type: "controls_if",
+                    }
+                ]
+            }
+        ]
+    }
+})
+
+const blocklyArea = document.getElementById("blocklyArea")!
+const blocklyDiv = document.getElementById("blocklyDiv")!
+const workspace = Blockly.inject(
+    blocklyDiv,
+    {
+        toolbox: document.getElementById("toolbox")!
+    }
+)
+const onresize = () => {
+    let element = blocklyArea!
+    let x = 0
+    let y = 0
+    do {
+        x += element.offsetLeft
+        y += element.offsetTop
+        element = element.offsetParent as HTMLElement
+    } while (element)
+    blocklyDiv.style.left = x + "px"
+    blocklyDiv.style.top = y + "px"
+    blocklyDiv.style.width = blocklyArea.offsetWidth + "px"
+    blocklyDiv.style.height = blocklyArea.offsetHeight + "px"
+    Blockly.svgResize(workspace)
+}
+addEventListener("resize", onresize, false)
