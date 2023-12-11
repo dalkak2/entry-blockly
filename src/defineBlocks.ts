9c58ce251c6b0b7e4ee2c33edbf0154dcf89833f
Gnlow <gnlow@protonmail.com>
2023-12-11 10:55:06 +0900

    hmm

--- src/defineBlocks.ts
+++ src/defineBlocks.ts
@@ -5,22 +5,27 @@ const jsGenerator = new Blockly.Generator("js")
 Blockly.defineBlocksWithJsonArray([
     {
         type: "when_run_button_click",
-        message0: "시작하기 버튼을 클릭했을 때\n%1",
-        args0: [
-            {
-                type: "input_statement",
-                name: "NAME",
-            }
-        ],
+        message0: "시작하기 버튼을 클릭했을 때",
+        nextStatement: null,
         colour: 135,
     }
 ])
 
 jsGenerator.forBlock["when_run_button_click"] =
     (block, generator) => {
+        console.log(block.getChildren(false))
+        block.nextConnection
         return `hi(${
-            generator.statementToCode(block, "NAME")
+            block.getNextBlock()
+                ? generator.blockToCode(block.getNextBlock()!)
+                : ""
         })`
+    }
+jsGenerator.forBlock["controls_if"] =
+    (block, generator) => {
+        return `if` + (block.getNextBlock()
+                ? generator.blockToCode(block.getNextBlock()!)
+                : "")
     }
 
 export { jsGenerator }
