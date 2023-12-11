49528101e19a62f55fbe0a1e4e94169d3e03a907
Gnlow <gnlow@protonmail.com>
2023-12-11 13:12:39 +0900

    load entry blocks

--- src/defineBlocks.ts
+++ src/defineBlocks.ts
@@ -2,15 +2,6 @@ import { Blockly } from "./deps/blockly.ts"
 
 const jsGenerator = new Blockly.Generator("js")
 
-Blockly.defineBlocksWithJsonArray([
-    {
-        type: "when_run_button_click",
-        message0: "시작하기 버튼을 클릭했을 때",
-        nextStatement: null,
-        colour: 135,
-    }
-])
-
 jsGenerator.forBlock["when_run_button_click"] =
     (block, generator) => {
         console.log(block.getChildren(false))
@@ -30,6 +21,14 @@ jsGenerator.forBlock["controls_if"] =
 
 export { jsGenerator }
 
+import type { CategoryData } from "https://deno.land/x/entry_block_data@0.1.0/type.ts"
+
+const entryBlockData = await fetch("https://deno.land/x/entry_block_data@0.1.0/data.json").then(x => x.json()) as CategoryData[]
+
+console.log(
+    entryBlockData
+)
+
 export const toolbox = {
     kind: "categoryToolbox",
     contents: [
@@ -47,5 +46,36 @@ export const toolbox = {
                 },
             ]
         },
+        ...entryBlockData.map(({ category, blocks }) => ({
+            kind: "category",
+            name: category,
+            contents: blocks
+                .filter(({ template }) => template)
+                .map(({ blockName }) => ({
+                    kind: "block",
+                    type: blockName,
+                }))
+        }))
     ]
 }
+
+Blockly.defineBlocksWithJsonArray([
+    /*
+    {
+        type: "when_run_button_click",
+        message0: "시작하기 버튼을 클릭했을 때",
+        nextStatement: null,
+        colour: 135,
+    },
+    */
+    ...entryBlockData
+        .map(({ blocks }) => blocks)
+        .flat()
+        .filter(({ template }) => template)
+        .map(block => ({
+            type: block.blockName,
+            message0: block.template?.replaceAll("%", ""),
+            colour: block.color,
+            nextStatement: null,
+        }))
+])
