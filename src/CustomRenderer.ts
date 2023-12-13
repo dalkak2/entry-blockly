a4d66b98f7999d339a66119c1fbe45fe567f4310
Gnlow <gnlow@protonmail.com>
2023-12-13 11:38:27 +0900

    customize notch

--- src/CustomRenderer.ts
+++ src/CustomRenderer.ts
@@ -3,7 +3,47 @@ import { Blockly } from "./deps/blockly.ts"
 class CustomConstantProvider extends Blockly.zelos.ConstantProvider {
     constructor() {
         super()
-        //this.NOTCH_HEIGHT = 20
+    }
+    override makeNotch() {
+        /* https://github.com/google/blockly/blob/develop/core/renderers/zelos/constants.ts */
+        // const width = this.NOTCH_WIDTH
+        // const height = this.NOTCH_HEIGHT
+    
+        return {
+            type: this.SHAPES.NOTCH,
+            width: 10,
+            height: 8,
+            pathLeft: ` l 8 8 v -6 a 2 2 0 0 1 2 -2 `,
+            pathRight: ` a 2 2 0 0 0 -2 2 v 6 l -8 -8 `,
+        }
+      }
+}
+
+class CustomDrawer extends Blockly.zelos.Drawer {
+    constructor(
+        block: Blockly.BlockSvg,
+        info: Blockly.zelos.RenderInfo,
+    ) {
+        super(block, info)
+    }
+
+    override draw() {
+        super.draw()
+    }
+    override drawOutline_() {
+        if (
+            this.info_.outputConnection &&
+            this.info_.outputConnection.isDynamicShape &&
+            !this.info_.hasStatementInput &&
+            !this.info_.bottomRow.hasNextConnection
+        ) {
+            this.drawFlatTop_();
+            this.drawRightDynamicConnection_();
+            this.drawFlatBottom_();
+            this.drawLeftDynamicConnection_();
+        } else {
+            super.drawOutline_();
+        }
     }
 }
 
@@ -13,6 +53,12 @@ class CustomRenderer extends Blockly.zelos.Renderer {
     }
     makeConstants_() {
         return new CustomConstantProvider()
+    }
+    makeDrawer_(
+        block: Blockly.BlockSvg,
+        info: Blockly.zelos.RenderInfo,
+    ) {
+        return new CustomDrawer(block, info)
     }
 }
 
